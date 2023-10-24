b770084be443be2611d1d6f6d68b60c6d015666e
Letian <1350171526@qq.com>
2023-10-24 17:59:36 +0800

    组件创建

--- zheng-jiu-zhe/src/views/Layout/index.vue
+++ zheng-jiu-zhe/src/views/Layout/index.vue
@@ -3,10 +3,21 @@
 </script>
 
 <template>
- a
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 
+  .common-layout{
+    color: #000;
+  }
 
 </style>
