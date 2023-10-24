0e7b6a2d524923244a0c440fdc88afab6c46429d
Letian <1350171526@qq.com>
2023-10-24 14:19:08 +0800

    第三方插件引入

---
+++ zheng-jiu-zhe/src/views/Layout/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import {ref} from 'vue'
+
+</script>
+
+<template>
+  <h1>layout</h1>
+
+</template>
+
+<style lang="scss" scoped>
+  h1{
+    margin: 0;
+    padding : 0;
+  }
+</style>
