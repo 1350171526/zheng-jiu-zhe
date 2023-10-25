990232c729e8b46eca6d9e9386632bb7fdaa0438
Letian <1350171526@qq.com>
2023-10-25 14:36:43 +0800

    aside优化

--- zheng-jiu-zhe/src/views/Layout/components/Aside.vue
+++ zheng-jiu-zhe/src/views/Layout/components/Aside.vue
@@ -1,29 +1,51 @@
 <template>
   <div class="Aside">
-    <div>首页</div>
-    <div>热门视频</div>
-    <div>体育频道</div>
-    <span class=" iconfont icon-remen"></span>
+    <div v-for="(item,index) in State" 
+    :key="item.id"
+     @click="activeindex=index"
+     :class="{Color:index===activeindex}"
+     
+     >
+      <span :class="item.icon"></span>
+      <span>{{ item.name }}</span>
+    </div> 
+   
   </div>
 </template>
 
-<script>
-  export default {
-    
-  }
+<script setup>
+import { ref } from "vue"
+const activeindex = ref('')
+const State = ref([
+  {id:1,name:'首页',icon:'iconfont icon-shouye'},
+  {id:2,name:"热门",icon:'iconfont icon-remen'},
+  {id:3,name:"体育",icon:'iconfont icon-tiyu'}
+])
+
+
+
+
 </script>
 
 <style lang="scss" scoped>
-div{
-  height: 50px;
-  text-align: center;
-  line-height: 50px;
- border-radius: 15px;
-  :hover{
-    background-color: #333;
-  }
-  
+.Aside div {  
+  height: 50px;  
+  text-align: center;  
+  line-height: 50px;  
+  border-radius: 15px;  
+  &:hover {  
+  background-color: rgba(255, 255, 255, 0.2);  
+}  
+&.Color{
+  background-color: rgba(255, 255, 255, 0.4);  
+  color: #fff;
 }
+} 
+
+.Aside div span {  
+  padding: 0 5px;  
+} 
+
 
 
 
